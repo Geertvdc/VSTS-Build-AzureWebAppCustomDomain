86791ab708c15b918207f4c108983517ccaae343
Geert van der Cruijsen <geert@vdcruijsen.net>
2017-04-26 08:23:59 +0200

    added powershell to set host name

--- geertvdc-vsts-azurewebapp-addhostname/addHostname.ps1
+++ geertvdc-vsts-azurewebapp-addhostname/addHostname.ps1
@@ -2,12 +2,23 @@ Trace-VstsEnteringInvocation $MyInvocation
 
 $ConnectedServiceName = Get-VstsInput -Name ConnectedServiceName -Require
 $WebAppName = Get-VstsInput -Name WebAppName -Require
-$domainName = Get-VstsInput -Name domainName -Require
+$ResourceGroupName = Get-VstsInput -Name ResourceGroupName -Require
+$DeployToSlotFlag = Get-VstsInput -Name DeployToSlotFlag -Require
+$SlotName = Get-VstsInput -Name SlotName -Require
+$DomainName = Get-VstsInput -Name DomainName -Require
 
 # Initialize Azure.
 Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
 Initialize-Azure
 Write-Output "Azure Initialized"
 
-$webapp = Get-AzureRmWebApp -name "$WebAppName"
-$resourceGroup = $webapp.ResourceGroup
+$newHosts = @($DomainName)
+
+if($DeployToSlotFlag){
+
+	$result = Set-AzureRmWebApp -name "$WebAppName" -slot $SlotName -ResourceGroupName $ResourceGroupName -HostNames $newHosts
+}
+else {
+	$result = Set-AzureRmWebApp -name "$WebAppName" -HostNames $newHosts
+}
+
